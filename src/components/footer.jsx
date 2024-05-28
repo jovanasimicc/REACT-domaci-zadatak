d54e1c274f4a75bb5d78cf6373e1efdd047497b5
jovanasimicc <118056243+jovanasimicc@users.noreply.github.com>
2024-05-28 23:30:17 +0200

    commit

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -1,35 +1,35 @@
 import React from "react";
 import "./footer.css"
 
-export const Footer = () =>{
+export const Footer = () => {
     return (
-    <div className="all-footer">
-        <section className="footer">
-            <div className="footer-row">
-                <div className="footer-column">
-                    <div className="contact">
-                        <h3>Kontakt</h3>
-                        <ul>
-                            <li>Uzun Mirkova 24, Valjevo, Srbija</li>
-                            <li>+381 14 565 211</li>
-                            <li>knjizaraatina@gmail.com</li>
-                        </ul>
+        <div className="all-footer">
+            <section className="footer">
+                <div className="footer-row">
+                    <div className="footer-column">
+                        <div className="contact">
+                            <h3>Kontakt</h3>
+                            <ul>
+                                <li>Uzun Mirkova 24, Valjevo, Srbija</li>
+                                <li>+381 14 565 211</li>
+                                <li>knjizaraatina@gmail.com</li>
+                            </ul>
+                        </div>
+                    </div>
+
+                    <div className="footer-column">
+                        <h2>Newsletter</h2>
+                        <form>
+                            <input type="email" placeholder="Unesite email adresu..." required /> <br />
+                            <button type="submit" className="btnPrijaviSe">Prijavite se</button>
+                        </form>
                     </div>
                 </div>
 
-                <div className="footer-column">
-                    <h2>Newsletter</h2>
-                    <form>
-                        <input type="email" placeholder="Unesite email adresu..." required/> <br />
-                        <button type="submit" className="btnPrijaviSe">Prijavite se</button>
-                    </form>
-                </div>
-            </div>
+                <hr />
+                <p className="copyright">Copyright © 2023. Knjižara "Atina"</p>
 
-            <hr />
-            <p className="copyright">Copyright © 2023. Knjižara "Atina"</p>
-
-        </section>
-    </div>
+            </section>
+        </div>
     );
-  };
+};
